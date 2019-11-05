cb946356d605310fd02ce1d759725ea937dff26c
yaoweiyu <yaoweiyu@peilian.com>
2019-11-05 19:15:44 +0800

    修改bug

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: 'vine_xiaoshuo',
   port: 3000,
-  address: location.host.includes('47.105.109.192') ? 'http://47.105.109.192' : 'localhost'
+  address: 'http://47.105.109.192'
 }
