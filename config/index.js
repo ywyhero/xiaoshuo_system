b4598ff685a33cc00dbbf9c88b9aa01aa669a4ef
yaoweiyu <yaoweiyu@peilian.com>
2019-11-12 10:49:02 +0800

    修改地址

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   secret: 'vine_xiaoshuo',
   port: 3000,
-  // address: 'http://192.168.44.132',
+  // address: 'http://192.168.44.96',
   // address: 'http://www.vinekan.com',
   address: 'http://47.105.109.192',
   // prodport: 80,
