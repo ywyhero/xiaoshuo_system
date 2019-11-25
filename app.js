2116bd1598f346e8e756cf5d0ae4f57e9ff1dcb2
yaoweiyu <yaoweiyu@peilian.com>
2019-11-25 17:47:30 +0800

    换地址

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const jwtKoa  = require('koa-jwt');      // 用于路由权限控制
 const port = process.env.PORT || config.port;
 const jwtMethod = require('./utils/index.js');
 onerror(app)
-const origin = config.address = 'http://www.vinekan.com' ? `${config.address}` : `${config.address}:${config.prodport}`;
+const origin = config.address === 'http://www.vinekan.com' ? `${config.address}` : `${config.address}:${config.prodport}`;
 app.use(cors({
     origin:   function(ctx) { //设置允许来自指定域名请求
-        const whiteList = [`${config.address}:3080`,`${config.address}:3000`,'http://www.vinekan.com:3000','https://www.vinekan.com:3000']; //可跨域白名单
+        const whiteList = [`${config.address}:3080`,`${config.address}:3000`]; //可跨域白名单
         let url = ctx.header.referer && ctx.header.referer.substr(0, ctx.header.referer.length - 1);
         if(whiteList.includes(url)){
+            console.log(url)
             return url //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
         }
         return origin //默认允许本地请求3000端口可跨域
