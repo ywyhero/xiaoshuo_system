31baf8a730e10abcf501560ff0d7f110541f1cb7
yaoweiyu <yaoweiyu@peilian.com>
2019-11-25 23:16:52 +0800

    youhua

--- controller/db.js
+++ controller/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/xiaoshuo',{
+mongoose.connect('mongodb://vine:900808&127.0.0.1:27017/xiaoshuo',{
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
