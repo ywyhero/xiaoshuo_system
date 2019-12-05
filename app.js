969ad3fc0545f5137a660fc4c2c43856c17d6633
yaoweiyu <yaoweiyu@peilian.com>
2019-12-05 11:28:25 +0800

    优化

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const jwtKoa  = require('koa-jwt');      // 用于路由权限控制
 const port = process.env.PORT || config.port;
 const jwtMethod = require('./utils/index.js');
 onerror(app)
-const origin = config.address === 'http://www.vinekan.com' ? config.address : `${config.address}:${config.prodport}`;
+const origin = config.address === 'https://www.vinekan.com' ? config.address : `${config.address}:${config.prodport}`;
 app.use(cors({
     origin:   function(ctx) { //设置允许来自指定域名请求
-        const whiteList = [`${config.address}:3080`,`${config.address}:8080`,'http://www.vinekan.com']; //可跨域白名单
+        const whiteList = [`${config.address}:3080`,`${config.address}:8080`,'https://www.vinekan.com']; //可跨域白名单
         let url = ctx.header.referer && ctx.header.referer.substr(0, ctx.header.referer.length - 1);
         if(whiteList.includes(url)){
             return url //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
